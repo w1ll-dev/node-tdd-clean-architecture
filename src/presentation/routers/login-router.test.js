ce93dc220d7ca4ebdbff9a6b0b2c87ef0bfed81e
w1ll-dev <w1llmarcio.dev@hotmail.com>
2020-07-23 17:09:07 -0300

    ensure LoginRouter calls EmailValidator with correct email

--- src/presentation/routers/login-router.test.js
+++ src/presentation/routers/login-router.test.js
@@ -37,6 +37,7 @@ const makeEmailValidatorWithError = () => {
 const makeEmailValidator = () => {
   class EmailValidatorSpy {
     isValid (email) {
+      this.email = email
       return this.isEmailValid
     }
   }
@@ -250,5 +251,17 @@ describe('Login Router', () => {
     const httpResponse = await sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body).toEqual(new ServerError())
+  })
+
+  test('Should call EmailValidator with correct email', async () => {
+    const { sut, emailValidatorSpy } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      }
+    }
+    sut.route(httpRequest)
+    expect(emailValidatorSpy.email).toBe(httpRequest.body.email)
   })
 })
