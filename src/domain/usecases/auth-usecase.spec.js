a51e60aca83a66ab53c6e0b5e542e42984d6d108
w1ll-dev <w1llmarcio.dev@hotmail.com>
2020-07-27 17:56:20 -0300

    ensure AuthUseCase calls LoadUserByEmailRepository with correct email

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -1,24 +1,47 @@
 const { MissingParamError } = require('../../utils/errors/')
 
-class AuthUseCase {
-  async auth (email, password) {
-    if (!email) {
-      throw new MissingParamError('email')
+const makeSut = () => {
+  class AuthUseCase {
+    constructor (loadUserByEmailRepository) {
+      this.loadUserByEmailRepository = loadUserByEmailRepository
     }
-    if (!password) {
-      throw new MissingParamError('password')
+
+    async auth (email, password) {
+      if (!email) {
+        throw new MissingParamError('email')
+      }
+      if (!password) {
+        throw new MissingParamError('password')
+      }
+      this.loadUserByEmailRepository.load(email)
     }
+  }
+  class LoadUserByEmailRepository {
+    async load (email) {
+      this.email = email
+    }
+  }
+  const loadUserByEmailRepository = new LoadUserByEmailRepository()
+  const sut = new AuthUseCase(loadUserByEmailRepository)
+  return {
+    sut,
+    loadUserByEmailRepository
   }
 }
 describe('AuthUseCase', () => {
   test('Shoul throw if no email is provided', async () => {
-    const sut = new AuthUseCase()
+    const { sut } = makeSut()
     const promise = sut.auth()
     expect(promise).rejects.toThrow(new MissingParamError('email'))
   })
   test('Shoul throw if no password is provided', async () => {
-    const sut = new AuthUseCase()
+    const { sut } = makeSut()
     const promise = sut.auth('any_email@mail.com')
     expect(promise).rejects.toThrow(new MissingParamError('password'))
+  })
+  test('Shoul call LoadUserByEmailRepository with correct email', async () => {
+    const { sut, loadUserByEmailRepository } = makeSut()
+    sut.auth('any_email@mail.com', 'any_password')
+    expect(loadUserByEmailRepository.email).toBe('any_email@mail.com')
   })
 })
